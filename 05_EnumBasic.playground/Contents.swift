f9a1a168bf254409749760352fb9752cf8944340
sonnyshih <sonnyshih@gmail.com>
2020-06-02 10:04:34 +0800

    add enum demo

---
+++ 05_EnumBasic.playground/Contents.swift
@@ -0,0 +1,33 @@
+// Enum = Enumeration
+enum Direction {
+    case east
+    case west
+    case south
+    case north
+}
+
+let myDirection:Direction = Direction.east
+print(myDirection)
+
+
+func getDirection (direction: Direction) {
+ 
+    switch direction {
+    case Direction.east:
+        print("go east")
+     
+    case Direction.west:
+        print("go west")
+        
+    case .south:
+        print("got south")
+        
+    case .north:
+        print("got North")
+        
+    default:
+        print("None")
+    }
+}
+
+getDirection(direction: .east)
