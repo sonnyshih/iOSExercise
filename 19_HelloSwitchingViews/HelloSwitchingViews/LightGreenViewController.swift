658d7eb9d500afcc02a273d442d845e9a6f242ba
sonnyshih <sonnyshih@gmail.com>
2020-06-04 15:36:09 +0800

    add switch views Demo

---
+++ 19_HelloSwitchingViews/HelloSwitchingViews/LightGreenViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LightGreenViewController.swift
+//  HelloSwitchingViews
+//
+//  Created by sonny on 2020/6/4.
+//  Copyright © 2020 sonny. All rights reserved.
+//
+
+import UIKit
+
+class LightGreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
